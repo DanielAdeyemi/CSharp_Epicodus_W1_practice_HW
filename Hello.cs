220caacef100de2443857612fdcb350ab03a7b96
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-21 19:49:50 -0800

    add class

--- Hello.cs
+++ Hello.cs
@@ -1,78 +1,115 @@
 using System;
 using System.Collections.Generic;
 
-class Phonebook
+public class Car
 {
-  public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+}
 
-  static void Main()
+public class Program //UI
+{
+  public static void Main()
   {
-    Console.WriteLine("MAIN MENU");
-    Console.WriteLine("Would you like to add a person to your phone book? ['Y' for yes, 'Enter' for no]");
-    string answer = Console.ReadLine();
-    if (answer == "Y" || answer == "y")
-    {
-      AddContact();
-    }
-    else
-    {
-      Console.WriteLine("Would you like to lookup a number in your phone book? ['Y' for yes, 'Enter' for no]");
-      string lookUpAnswer = Console.ReadLine();
-      if (lookUpAnswer == "Y" || lookUpAnswer == "y")
-      {
-        LookUpContact();
-      }
-      else
-      {
-        Console.WriteLine("Are you finished with this program? ['Y' for yes, 'Enter' for no]");
-        string finishedAnswer = Console.ReadLine();
-        if (finishedAnswer == "Y" || finishedAnswer == "y")
-        {
-          Console.WriteLine("Goodbye.");
-        }
-        else
-        {
-          Main();
-        }
-      }
-    }
-  }
+    Car volkswagen = new Car();
+    volkswagen.MakeModel = "1974 Volkswagen Thing";
+    volkswagen.Price = 1100;
+    volkswagen.Miles = 368792;
 
-  static void AddContact()
-  {
-    Console.WriteLine("NEW CONTACT");
-    Console.WriteLine("Enter a new contacts name");
-    string name = Console.ReadLine();
-    Console.WriteLine("Enter a new contacts phone number");
-    string number = Console.ReadLine();
-    if (phoneNumbers.ContainsKey(name))
-    {
-      Console.WriteLine("That person is already in your phonebook. Their number is " + phoneNumbers[name]);
-    }
-    else
-    {
-      phoneNumbers.Add(name, number);
-    }
-    Main();
-  }
+    Car yugo = new Car();
+    yugo.MakeModel = "1980 Yugo Koral";
+    yugo.Price = 700;
+    yugo.Miles = 56000;
 
-  static void LookUpContact()
-  {
-    Console.WriteLine("CONTACT LOOKUP");
-    Console.WriteLine("Whose number would you like to look up?");
-    string friend = Console.ReadLine();
-    if (phoneNumbers.ContainsKey(friend))
-    {
-      string value = phoneNumbers[friend];
-      Console.WriteLine(friend + "\'s phone number is " + value);
-    }
-    else
-    {
-      Console.WriteLine("That person is not in your phone book.");
-    }
-    Main();
+    Car ford = new Car();
+    ford.MakeModel = "1988 Ford Country Squire";
+    ford.Price = 1400;
+    ford.Miles = 239001;
+
+    Car amc = new Car();
+    amc.MakeModel = "1976 AMC Pacer";
+    amc.Price = 400;
+    amc.Miles = 198000;
   }
 }
+
+
+
+
+
+// class Phonebook
+// {
+//   public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+//   static void Main()
+//   {
+//     Console.WriteLine("MAIN MENU");
+//     Console.WriteLine("Would you like to add a person to your phone book? ['Y' for yes, 'Enter' for no]");
+//     string answer = Console.ReadLine();
+//     if (answer == "Y" || answer == "y")
+//     {
+//       AddContact();
+//     }
+//     else
+//     {
+//       Console.WriteLine("Would you like to lookup a number in your phone book? ['Y' for yes, 'Enter' for no]");
+//       string lookUpAnswer = Console.ReadLine();
+//       if (lookUpAnswer == "Y" || lookUpAnswer == "y")
+//       {
+//         LookUpContact();
+//       }
+//       else
+//       {
+//         Console.WriteLine("Are you finished with this program? ['Y' for yes, 'Enter' for no]");
+//         string finishedAnswer = Console.ReadLine();
+//         if (finishedAnswer == "Y" || finishedAnswer == "y")
+//         {
+//           Console.WriteLine("Goodbye.");
+//         }
+//         else
+//         {
+//           Main();
+//         }
+//       }
+//     }
+//   }
+
+//   static void AddContact()
+//   {
+//     Console.WriteLine("NEW CONTACT");
+//     Console.WriteLine("Enter a new contacts name");
+//     string name = Console.ReadLine();
+//     Console.WriteLine("Enter a new contacts phone number");
+//     string number = Console.ReadLine();
+//     if (phoneNumbers.ContainsKey(name))
+//     {
+//       Console.WriteLine("That person is already in your phonebook. Their number is " + phoneNumbers[name]);
+//     }
+//     else
+//     {
+//       phoneNumbers.Add(name, number);
+//     }
+//     Main();
+//   }
+
+//   static void LookUpContact()
+//   {
+//     Console.WriteLine("CONTACT LOOKUP");
+//     Console.WriteLine("Whose number would you like to look up?");
+//     string friend = Console.ReadLine();
+//     if (phoneNumbers.ContainsKey(friend))
+//     {
+//       string value = phoneNumbers[friend];
+//       Console.WriteLine(friend + "\'s phone number is " + value);
+//     }
+//     else
+//     {
+//       Console.WriteLine("That person is not in your phone book.");
+//     }
+//     Main();
+//   }
+// }
 
 // class HotelCost
 // {
