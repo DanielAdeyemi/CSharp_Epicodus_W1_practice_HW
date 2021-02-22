30fe192b7d4d87b5e08a88da6ebecb5a571a22c9
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-21 17:42:30 -0800

    add bool into branching

--- Hello.cs
+++ Hello.cs
@@ -1,21 +1,43 @@
 using System;
 
-class FirstLetterChecker
+class HotelCost
 {
-    static void Main() 
+    static void Main()
     {
-      Console.WriteLine("What is your name?");
-      string userName = Console.ReadLine();
-      if (userName.StartsWith("Z"))
+      Console.WriteLine("In what season are you booking a stay?");
+      string season = Console.ReadLine();
+      Console.WriteLine("On the weekend or weeknight?");
+      string day = Console.ReadLine();
+      bool summer = season == "summer";
+      bool weekend = day == "weekend";
+      if (summer && weekend) 
       {
-          Console.WriteLine("Your name starts with Z");
-      }
-      else
-      {
-          Console.WriteLine("Your name doesn't start with a Z!");
+        Console.WriteLine("Your stay is probably going to be pretty expensive. It is both - peak travel season and the weekend!");
+        else
+        {
+            Console.WriteLine("Your stay might be expensive but it could be worse.");
+        }
       }
     }
 }
+
+
+// class FirstLetterChecker
+// {
+//     static void Main() 
+//     {
+//       Console.WriteLine("What is your name?");
+//       string userName = Console.ReadLine();
+//       if (userName.StartsWith("Z"))
+//       {
+//           Console.WriteLine("Your name starts with Z");
+//       }
+//       else
+//       {
+//           Console.WriteLine("Your name doesn't start with a Z!");
+//       }
+//     }
+// }
 
 // class Program
 // {
