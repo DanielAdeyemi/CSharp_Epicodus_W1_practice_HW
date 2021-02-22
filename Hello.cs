feefb884e7d6d29b03a78ca8b29a10de505bc898
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-21 19:54:02 -0800

    add foreach loop to display cars

--- Hello.cs
+++ Hello.cs
@@ -31,6 +31,13 @@ public class Program //UI
     amc.MakeModel = "1976 AMC Pacer";
     amc.Price = 400;
     amc.Miles = 198000;
+
+    List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
+
+    foreach(Car automobile in Cars)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
   }
 }
 
