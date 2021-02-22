c0513bd694f099d6625c0213d619052058e824b0
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-21 17:04:23 -0800

    use branching and startswith method

--- Hello.cs
+++ Hello.cs
@@ -1,5 +1,22 @@
 using System;
 
+class FirstLetterChecker
+{
+    static void Main() 
+    {
+      Console.WriteLine("What is your name?");
+      string userName = Console.ReadLine();
+      if (userName.StartsWith("Z"))
+      {
+          Console.WriteLine("Your name starts with Z");
+      }
+      else
+      {
+          Console.WriteLine("Your name doesn't start with a Z!");
+      }
+    }
+}
+
 // class Program
 // {
 //   static void Main()  
@@ -13,21 +30,21 @@ using System;
 //   }
 // }
     
-class RatedR
-{
-    static void Main() 
-    {
-    Console.WriteLine("How old are you?");
-    string sUserAge = Console.ReadLine();
-    int iUserAge = int.Parse(sUserAge);
+// class RatedR
+// {
+//     static void Main() 
+//     {
+//     Console.WriteLine("How old are you?");
+//     string sUserAge = Console.ReadLine();
+//     int iUserAge = int.Parse(sUserAge);
 
-    if (iUserAge >= 17)
-    {
-        Console.WriteLine("You can see the movie! ");
-    }
-    else
-    {
-        Console.WriteLine("I am sorry, you are too young to see the movie.");
-    }
-    }
-}
+//     if (iUserAge >= 17)
+//     {
+//         Console.WriteLine("You can see the movie! ");
+//     }
+//     else
+//     {
+//         Console.WriteLine("I am sorry, you are too young to see the movie.");
+//     }
+//     }
+// }
